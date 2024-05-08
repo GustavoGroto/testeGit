546bf8c8781670bd4b77a78c85e86858d4b303bd
GustavoGroto <groto.gustavo@gmail.com>
2024-05-08 14:29:41 -0300

    commit

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <body>
  
 <?php
+//Gustavo Groto, Felipe Neves, Wesley dos Santos, Alexandre Almeida, Vitor Nathan, André Marchiori, Raphael de Jesus
+
 /*1 -Construir um algoritmo que leia 2 números e efetue a adição. Caso o valor somado seja maior que 20, este deverá ser apresentando somando-se a ele mais 8; caso o valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-se 5.
 $n1 = 11; //numero a ser alterado
 $n2 = 10; //numero a ser alterado
